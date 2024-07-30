d8b354bf0471cb7912d35af2c85d2bb956236b75
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2024-07-30 13:33:32 +0000

    Add: Placement of ships

--- run.py
+++ run.py
@@ -8,3 +8,21 @@ SHIP = 'S'
 HIT = 'X'
 MISS = 'O'
 
+# initialize the board
+
+board = [[EMPTY for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+
+# placing the ships
+
+def place_ship(board, x, y, length):
+    for i in range(length):
+        if x+i < BOARD_SIZE:
+            board[x+i][y] = SHIP
+
+def random_ship_placement(board):
+    import random
+    for _ in range(5):  # Max 5 ships
+        length = random.randint(1, 3)  # Max length 3
+        x = random.randint(0, BOARD_SIZE - 1)
+        y = random.randint(0, BOARD_SIZE - 1)
+        place_ship(board, x, y, length)
